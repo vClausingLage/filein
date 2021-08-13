f2cebc1d3f71da1b53bc8b5edffcd2d7d273d456
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2021-08-13 09:14:37 +0200

    nlp

--- App/components/header.php
+++ App/components/header.php
@@ -3,6 +3,7 @@ echo '<header>';
 echo '<nav>';
 echo '<a href="/../index.php">Home</a>';
 echo '<a href="/../blog.php">Blog</a>';
+echo '<a href="/../processing/morphology_reducer.php">NLP</a>';
 echo '<a href="/../processing/data_logger.php">Data Logger</a>';
 echo '<a href="/../achilleus.php">Achilleus Tatios</a>';
 echo '<a href="/../about.php">About</a>';
