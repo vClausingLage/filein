b5528d10ec42bfb0f59480c31234b2c6fad30e91
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2021-09-11 19:06:56 +0200

    aeschylus

--- TextToData/visualizing_aeschylus/viAeschyl.py
+++ TextToData/visualizing_aeschylus/viAeschyl.py
@@ -1,7 +1,47 @@
 import re
 import json
 
-with open('aeschylus.txt', 'r') as file:
-    text = file.read()
+# To Do 
+# aus Text alle Elemente, die auf - enden mit folgendem verbinden
 
-print(text)
+with open('aeschylus.txt', 'r', encoding='utf8') as file:
+    text = file.read()
+    text = json.loads(text)
+
+text_str = " ".join(text)
+
+def removeNoise(query_string):
+  noise_terms = 'μὲν τὴν δὲ καὶ ὁ τῆς τὸν τῶν δ Χορός γὰρ τε ἐν τὸ ὡς τ'
+  noise_terms = noise_terms.strip()
+  noise_terms = noise_terms.split()
+  for term in noise_terms:
+    re.sub(f'{term}', '', query_string)
+  return query_string
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    counts = sorted(counts.items(), key=lambda item: item[1])
+    return counts
+
+print(text_str)
+text_str = removeNoise(text_str)
+word_map = word_count(text_str)
+
+total = len(text)
+hatred = 0
+love = 0
+
+
+# print(total)
+# print(word_map)
+
+with open('aeschylData.json', 'w', encoding='utf8') as file:
+  data = {"author": 'Aeschylus', "total": total, "hatred": hatred, "love": love}
+  output = json.dumps(data, ensure_ascii=False)
+  file.write(output)
