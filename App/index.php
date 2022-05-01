22789eb8de07aee214a523fd2edf579975c34dd0
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2022-05-01 23:49:13 +0200

    'setup'

--- App/index.php
+++ App/index.php
@@ -10,6 +10,23 @@
 </head>
 <body>
 
+<?php
+function factorial($x) {
+  if ($x === 0) {
+    return 1;
+  }
+  else {
+    return $x * factorial($x - 1);
+  }
+}
+$num = 4;
+if ($num > 0) {
+  $result = factorial($num);
+  echo "The factorial of $num is $result";
+}
+?>
+
+
 <?php include 'components/header.php' ?>
 
 <div class="container">
