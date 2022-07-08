dd3b80976d458d79d65dc2b9874a6bb299472ff4
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2022-07-08 10:43:26 +0200

    'reassign'

--- App/components/blogposts.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo 'the posts';
-
-echo '<p>Blogsposts contain <ul> <li>id</id><li>date</id><li>name</id><li>title</id><li>post</id> </ul>';
-
-include 'connections.php'
-?>
--- App/components/footer.php
+++
@@ -1,5 +0,0 @@
-<?php 
-echo '<footer>';
-echo '<p>&copy; v. clausing-lage ' . date('Y') . '</p>';
-include 'impressum.php';
-echo '</footer>';
